38c115cc0d7589bdcb7bf3d283bad1a9fa8a8d06
Jamie Robertson <jamie.robertson@cellosignal.com>
2018-05-15 09:00:22 +0100

    tidied up styling

--- src/components/pokemon/pokemonDetail.js
+++ src/components/pokemon/pokemonDetail.js
@@ -59,26 +59,24 @@ class PokemonDetail extends Component {
 
     if (fetched) {
        content = <div className="pokemon-detail">
-         <Link to='/'>Back</Link>
+         <Link to='/'>&laquo; Back</Link>
          <h2>{poke.name} - #{poke.id}</h2>
          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`} alt="{poke.name}"/>
          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${poke.id}.png`} alt="{poke.name} Shiny variant"/>
          <h3>Stats:</h3>
-         <ul>
-          <li>height: {poke.height}</li>
-          <li>weight: {poke.weight}</li>
+         <ul className="pokemon-detail--stats">
+          <li>height: {poke.height}in</li>
+          <li>weight: {poke.weight}lb</li>
          </ul>
          <h3>Type:</h3>
-         <ul className="pokemon-detail--type">{poke.types.map((t, index) => <li key={t.type.name}>{t.type.name}</li>)}</ul>
+         <ul className="pokemon-detail--type">{poke.types.map((t, index) => <li key={t.type.name} className={`pill-${t.type.name}`}>{t.type.name}</li>)}</ul>         
          <h3>Weak against:</h3>
-         <ul className="pokemon-detail--weak">{type.damage_relations.double_damage_from.map((weakness, i) => <li key={weakness.name}>{weakness.name}</li>)}</ul>
+         <ul className="pokemon-detail--weak">{type.damage_relations.double_damage_from.map((weakness, i) => <li key={weakness.name} className={`pill-${weakness.name}`}>{weakness.name}</li>)}</ul>
          <h3>Strong against:</h3>
-         <ul className="pokemon-detail--strong">{type.damage_relations.double_damage_to.map((strength, i) => <li key={strength.name}>{strength.name}</li>)}</ul>
-         <h3>Also known as:</h3>
-         <ul className="pokemon-detail--alt-names">{type.names.map((n, i) => <li key={n.language.name}>{n.name} - Language: {n.language.name}</li>)}</ul>
-         <h3>Abilities:</h3>
+         <ul className="pokemon-detail--strong">{type.damage_relations.double_damage_to.map((strength, i) => <li key={strength.name} className={`pill-${strength.name}`}>{strength.name}</li>)}</ul>
+          <h3>Special Abilities:</h3>
          <ul className="pokemon-detail--abilities">{poke.abilities.map((a, i) => <li key={a.ability.name}>{a.ability.name}</li>)}</ul>
-         <Link to={`/pokemon/${poke.id + 1}`}>Next Pokempn</Link>
+         <Link to={`/pokemon/${poke.id + 1}`}>Next Pokemon &raquo;</Link>
        </div>
     } else if (loading && !fetched) {
       content = <p className="loading">
