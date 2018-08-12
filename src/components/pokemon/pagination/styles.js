81168a8bf0b7845651de907f36972e3fcca729f9
Jamie Robertson <jamie@line.uk.com>
2018-08-12 22:23:42 +0100

    added padding t- pagination bottom

--- src/components/pokemon/pagination/styles.js
+++ src/components/pokemon/pagination/styles.js
@@ -3,8 +3,9 @@ import Styled from "styled-components";
 export const PaginationWrapper = Styled.footer`
     display: block;
     max-width: 90%;
-    margin: 0 auto 20px;
+    margin: auto;
     overflow: hidden;
+    padding-bottom: 20px;
 
     @media (min-width: 600px) {
         max-width: 600px;
