316a4732171e93b69c6cf7ce3452506d055c993d
Jamie Robertson <jamie@line.uk.com>
2018-11-08 22:22:26 +0000

    Updated footer external links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -9,27 +9,43 @@ export const Footer = () => {
         <a
           href="https://www.linkedin.com/in/jamie-robertson-8b75905b/"
           target="_BLANK"
+          rel="noopener noreferrer"
         >
           Jamie Robertson
         </a>{" "}
         with{" "}
-        <a href="https://reactjs.org" target="_BLANK">
+        <a href="https://reactjs.org" target="_BLANK" rel="noopener noreferrer">
           React
         </a>{" "}
         and <span>♥</span>.
       </p>
       <p>
         Content managed by{" "}
-        <a href="https://graphcms.com" target="_BLANK">
+        <a
+          href="https://graphcms.com"
+          target="_BLANK"
+          rel="noopener noreferrer"
+        >
           GraqhCMS
         </a>
         , images provided by{" "}
-        <a href="https://github.com/ZeChrales/PogoAssets" target="_BLANK">
+        <a
+          href="https://github.com/ZeChrales/PogoAssets"
+          target="_BLANK"
+          rel="noopener noreferrer"
+        >
           PogoAssets
         </a>
       </p>
       <p>
-        View project on <a href="https://github.com/ru7hl355/pokedex">Github</a>
+        View project on{" "}
+        <a
+          href="https://github.com/ru7hl355/pokedex"
+          target="_BLANK"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
       </p>
     </StyledFooter>
   );
